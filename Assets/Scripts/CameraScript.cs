27a9ac1730742c407044af78ff5d6f2eaaf66772
maxono1 <92861475+mjaesch@users.noreply.github.com>
2023-07-31 15:21:59 +0200

    feat: crosshair done

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraScript : MonoBehaviour
 {
@@ -13,13 +12,33 @@ public class CameraScript : MonoBehaviour
     public float yMax = 3f;
     public float sensitivity = 1;
 
+    public Image crosshair;
+
+    public float crosshairMovementMultiplier = 20;
+
+    private Vector3 initialPos;
+    private Vector3 newPos;
+    public float maxCrosshairX = 1f;
+    public float minCrosshairX = -1f;
+    public float maxCrosshairY = 1f;
+    public float minCrosshairY = -1f;
+
     // Start is called before the first frame update
     void Start()
     {
         vCam = GetComponent<CinemachineVirtualCamera>();
         composer= vCam.GetCinemachineComponent<CinemachineComposer>();
-        composer.m_TrackedObjectOffset.y = 0;
-        composer.m_TrackedObjectOffset.x = 0;
+        //composer.m_TrackedObjectOffset.y = 0;
+       // composer.m_TrackedObjectOffset.x = 0;
+
+        initialPos = crosshair.rectTransform.position;
+        newPos = crosshair.rectTransform.position;
+
+        maxCrosshairX += initialPos.x;
+        minCrosshairX += initialPos.x;
+        maxCrosshairY += initialPos.y;
+        minCrosshairY += initialPos.y;
+
     }
 
     // Update is called once per frame
@@ -31,8 +50,9 @@ public class CameraScript : MonoBehaviour
     // damping?
     void Update()
     {
-
-        composer.m_TrackedObjectOffset.y += Input.GetAxis("Mouse Y") / 4;
+        
+        /*
+        composer.m_TrackedObjectOffset.y += Input.GetAxis("Mouse Y") / 6;
         if(composer.m_TrackedObjectOffset.y < yMin)
         {
             composer.m_TrackedObjectOffset.y = yMin;
@@ -42,7 +62,8 @@ public class CameraScript : MonoBehaviour
             composer.m_TrackedObjectOffset.y = yMax;
         }
 
-        composer.m_TrackedObjectOffset.x += Input.GetAxis("Mouse X") / 4;
+        composer.m_TrackedObjectOffset.x += Input.GetAxis("Mouse X") / 6;
+        
         if (composer.m_TrackedObjectOffset.x < xMin)
         {
             composer.m_TrackedObjectOffset.x = xMin;
@@ -50,6 +71,29 @@ public class CameraScript : MonoBehaviour
         if (composer.m_TrackedObjectOffset.x > xMax)
         {
             composer.m_TrackedObjectOffset.x = xMax;
+        }*/
+
+
+        //TODO: die bounds ändern
+        //newPos.x += composer.m_TrackedObjectOffset.x * crosshairMovementMultiplier;
+        //newPos.y += composer.m_TrackedObjectOffset.y * crosshairMovementMultiplier;
+        newPos.x += Input.GetAxis("Mouse X") * crosshairMovementMultiplier;
+        if( newPos.x > maxCrosshairX)
+        {
+            newPos.x = maxCrosshairX;
+        } else if (newPos.x < minCrosshairX)
+        {
+            newPos.x = minCrosshairX;
         }
+        newPos.y += Input.GetAxis("Mouse Y") * crosshairMovementMultiplier;
+        if (newPos.y > maxCrosshairY)
+        {
+            newPos.y = maxCrosshairY;
+        }
+        else if (newPos.y < minCrosshairY)
+        {
+            newPos.y = minCrosshairY;
+        }
+        crosshair.rectTransform.position = newPos;
     }
 }
