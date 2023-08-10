752dffc8b87122328e1506c4cf151bd4ddf333cd
maxono1 <92861475+mjaesch@users.noreply.github.com>
2023-08-10 12:05:49 +0200

    feat: grappling works

--- Assets/Scripts/GrapplingController.cs
+++ Assets/Scripts/GrapplingController.cs
@@ -9,18 +9,10 @@ using Cinemachine;
 /// </summary>
 public class GrapplingController : MonoBehaviour
 {
-    [SerializeField] private CameraScript cameraScript;
     private LineRenderer lineRenderer;
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
-
-    [TagField]
-    [Tooltip("Obstacles with this tag will be ignored.  "
-        + "It's a good idea to set this field to the player's tag")]
-    public string IgnoreTag = string.Empty;
-
-    public Transform grappleTip, cam, car;
-    
+    public Transform grappleTip, car, AimTarget;
 
     [Header("Grappling Behaviour")]
     [Tooltip("How far to raycast to place the aim target")]
@@ -29,9 +21,6 @@ public class GrapplingController : MonoBehaviour
     public float spring =8.5f;
     public float damper = 7f;
     public float massScale = 4.5f;
-
-    [Header("Camera and Crosshair")]
-    public RectTransform ReticleImage;
 
     // Start is called before the first frame update
     void Start()
@@ -67,14 +56,8 @@ public class GrapplingController : MonoBehaviour
     private void StartGrapple()
     {
         RaycastHit hit;
-        Debug.Log(" cam fwd " + cam.forward);
-        Debug.Log(" xhair pos " + cameraScript.getCrosshairOffset().normalized);
-        
-
-        Vector3 directionOfGrapple = cam.forward + cameraScript.getCrosshairOffset().normalized;
-        directionOfGrapple.Normalize();
-        Debug.Log(" both " + directionOfGrapple);
-        if (Physics.Raycast(cam.position, directionOfGrapple, out hit, maxGrapDistance, whatIsGrappleable))
+       
+        if (Physics.Raycast(transform.position, (AimTarget.position - transform.position), out hit, maxGrapDistance, whatIsGrappleable))
         {
             grapplePoint = hit.point;
             joint = car.gameObject.AddComponent<SpringJoint>();
@@ -98,64 +81,6 @@ public class GrapplingController : MonoBehaviour
     {
         lineRenderer.positionCount = 0;
         Destroy(joint);
-    }
-    /// <summary>
-    /// Quelle: cinemachine MoveAimTarget.cs
-    /// 
-    /// </summary>
-    /// <param name="pos"></param>
-    /// <param name="camPos"></param>
-    /// <returns></returns>
-    private Vector3 GetProjectedAimTarget(Vector3 pos, Vector3 camPos)
-    {
-        var origin = pos;
-        var fwd = (pos - camPos).normalized;
-        pos += maxGrapDistance * fwd;
-        if (whatIsGrappleable != 0 && RaycastIgnoreTag(
-            new Ray(origin, fwd),
-            out RaycastHit hitInfo, maxGrapDistance, whatIsGrappleable))
-        {
-            pos = hitInfo.point;
-        }
-        return pos;
-    }
-    /// <summary>
-    /// Quelle: cinemachine MoveAimTarget.cs
-    /// </summary>
-    /// <param name="ray"></param>
-    /// <param name="hitInfo"></param>
-    /// <param name="rayLength"></param>
-    /// <param name="layerMask"></param>
-    /// <returns></returns>
-    private bool RaycastIgnoreTag(
-        Ray ray, out RaycastHit hitInfo, float rayLength, int layerMask)
-    {
-        const float PrecisionSlush = 0.001f;
-        float extraDistance = 0;
-        while (Physics.Raycast(
-            ray, out hitInfo, rayLength, layerMask,
-            QueryTriggerInteraction.Ignore))
-        {
-            if (IgnoreTag.Length == 0 || !hitInfo.collider.CompareTag(IgnoreTag))
-            {
-                hitInfo.distance += extraDistance;
-                return true;
-            }
-
-            // Ignore the hit.  Pull ray origin forward in front of obstacle
-            Ray inverseRay = new Ray(ray.GetPoint(rayLength), -ray.direction);
-            if (!hitInfo.collider.Raycast(inverseRay, out hitInfo, rayLength))
-                break;
-            float deltaExtraDistance = rayLength - (hitInfo.distance - PrecisionSlush);
-            if (deltaExtraDistance < PrecisionSlush)
-                break;
-            extraDistance += deltaExtraDistance;
-            rayLength = hitInfo.distance - PrecisionSlush;
-            if (rayLength < PrecisionSlush)
-                break;
-            ray.origin = inverseRay.GetPoint(rayLength);
-        }
-        return false;
     }
 
 }
